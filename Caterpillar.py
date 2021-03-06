52baac9c26d2dd13ff9a57818de42bd0e2487f01
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-03-06 10:44:21 +0100

    Update Caterpillar.py

--- Caterpillar.py
+++ Caterpillar.py
@@ -16,6 +16,13 @@ caterpillar.hideturtle()
 # create a leaf turtle
 leaf = t.Turtle()
 leaf_shape = ((0, 0), (14, 2), (18, 6), (20, 20), (6, 18), (2, 14)) # leaf coordinates
+t.register_shape('leaf', leaf_shape)
+leaf.shape('leaf')
+leaf.color('green')
+leaf.penup()
+leaf.hideturtle()
+leaf.speed(0)
+
 
 
 
