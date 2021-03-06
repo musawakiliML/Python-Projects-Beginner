faaffd6e1e73354023a2cd08a9b0017ea8a150ce
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-03-06 10:33:15 +0100

    Update Caterpillar.py

---
+++ Caterpillar.py
@@ -0,0 +1,11 @@
+import random
+import turtle as t
+
+t.bgcolor('yellow')
+
+caterpillar = t.Turtle()
+caterpillar.shape("square")
+caterpillar.color("red")
+caterpillar.speed(0)
+caterpillar.penup()
+caterpillar.hideturtle()
