d3e3818d7797a4d83cbd4129e3f5f90ad1d15ecd
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-03-06 10:59:19 +0100

    Update Caterpillar.py

--- Caterpillar.py
+++ Caterpillar.py
@@ -34,7 +34,44 @@ score_turtle = t.Turtle()
 score_turtle.hideturtle()
 score_turtle.speed(0)
 
+# Main loop
 
+# Creating placeholder functions
+
+def outside_window():
+    pass
+
+def game_over():
+    pass
+
+def display_score(current_score):
+    pass
+
+def place_leaf():
+    pass
+
+# Create the game Starter
+
+def start_game():
+    global game_started
+
+    if game_started:
+        return
+
+    game_started = True
+
+    score = 0
+    text_turtle.clear()
+
+    caterpillar_speed = 2
+    caterpillar_length = 3
+    caterpillar.shapesize(1, caterpillar_length, 1)
+    caterpillar.showturtle()
+    display_score(score)
+    place_leaf()
+
+    # Start moving
+    
 
 
 
